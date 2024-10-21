e63232a4e26666f261e42ecd0855dcdc79f51472
Asad <PS210896@edu.summacollege.nl>
2024-10-22 00:19:41 +0200

    updated "internships" views and updated "tags" model, controller, migration and seeder.

--- resources/views/internships/index.blade.php
+++ resources/views/internships/index.blade.php
@@ -4,46 +4,55 @@
 @section('content')
     <section class="mx-auto py-8 px-12 bg-base-100 rounded-md shadow-mdg">
 
-        <div class="w-full inline-flex">
+        <div class="w-full inline-flex px-6">
             <h1 class="text-4xl font-bold mb-6">Stages</h1>
 
             <div class="ml-auto inline-flex">
+                @livewire('search-bar')
 
-                <a class="btn btn-success" href="{{ route('internships.create') }}">
-                    <button>Creeer Stage</button>
-                </a>
+                @if (Auth::check() && Auth::user()->id == 1)
+                    <a class="btn btn-success ml-4" href="{{ route('internships.create') }}">
+                        <button>Creëer stage</button>
+                    </a>
+                @endif
             </div>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-6">
             @foreach ($internships as $internship)
                 <div class="card bg-base-100 shadow-xl">
                     <figure>
                         <a href="{{ route('internships.show', $internship->id) }}">
-                            <img src="{{ $internship->image_url }}" alt="Shoes" />
+                            <img src="{{ $internship->image_url }}" />
                         </a>
                     </figure>
                     <div class="card-body">
                         <h2 class="card-title">{{ $internship->course }}</h2>
-                        <p>{{ $internship->spaces_left }} Spaces Left</p>
-                        <p>{{ $internship->learning_path }} - Location: {{ $internship->location }}</p>
-                        <p><strong>Start Date:</strong> {{ \Carbon\Carbon::parse($internship->from)->format('F j, Y') }}</p>
-                        <p><strong>Compensation:</strong>
-                            {{ $internship->compensation ? '$' . number_format($internship->compensation, 2) : 'Unpaid' }}
+                        <p><strong>Studie:</strong> {{ $internship->learning_path }}</p>
+                        <p><strong>Locatie:</strong> {{ $internship->location }}</p>
+                        <p><strong>Van:</strong> {{ \Carbon\Carbon::parse($internship->from)->format('F j, Y') }}</p>
+                        <p><strong>Plekken beschikbaar:</strong> {{ $internship->spaces_left }}</a></p>
+                        <p><strong>Stagevergoeding:</strong>
+                            {{ $internship->compensation ? '€' . number_format($internship->compensation, 2) : 'Unpaid' }}
                         </p>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary w-full">Contact</button>
+                        <div class="card-actions justify-end mt-3">
+                            <a class="btn btn-primary w-full"
+                                href="{{ route('internships.show', $internship->id) }}"><button>Bekijk</button></a>
+                            {{-- <button class="btn btn-primary w-full">Contact</button> --}}
                             {{-- EDIT --}}
-                            <a class="btn btn-info w-full" href="{{ route('internships.edit', $internship->id) }}">
-                                <button>Wijzigen {{ $internship->name }}</button>
-                            </a>
+                            @if (Auth::check() && Auth::user()->id == 1)
+                                <a class="btn btn-info w-full" href="{{ route('internships.edit', $internship->id) }}">
+                                    <button>Wijzigen</button>
+                                </a>
 
-                            {{-- DELETE --}}
-                            <form class="w-full" action="{{ route('internships.destroy', $internship->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-error w-full" type="submit">Verwijderen {{ $internship->name }}</button>
-                            </form>
+                                {{-- DELETE --}}
+                                <form class="w-full" action="{{ route('internships.destroy', $internship->id) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-error w-full" type="submit">Verwijderen</button>
+                                </form>
+                            @endif
                         </div>
                     </div>
                 </div>
--- resources/views/internships/show.blade.php
+++ resources/views/internships/show.blade.php
@@ -8,15 +8,19 @@
         </a> --}}
 
         <div class="grid grid-cols-2 gap-6 p-6">
-        <img class="h-[300px]" src="{{ $internship->image ?? 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/683bde173714301.649497f801aa1.jpg' }}" alt="Internship Image">
-        <div>
-            <p class="text-2xl mb-6 capitalize">{{ $internship->course }}</p>
-            <p>Studie richting: {{ $internship->learning_path }}</p>
-            <p>Locatie: {{ $internship->location }}</p>
-            <p>Van: {{ $internship->from }}</p>
-            <p>Plekken beschikbaar: {{ $internship->spaces_left }}</a></p>
-            <p>Stagevergoeding: {{ $internship->compensation }}</p>
-        </div>
+            <img class="h-[300px]"
+                src="{{ $internship->image ?? 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/683bde173714301.649497f801aa1.jpg' }}"
+                alt="Internship Image">
+            <div>
+                <p class="text-2xl mb-6 capitalize font-bold">{{ $internship->course }}</p>
+                <p><strong>Studie:</strong> {{ $internship->learning_path }}</p>
+                <p><strong>Locatie:</strong> {{ $internship->location }}</p>
+                <p><strong>Van:</strong> {{ $internship->from }}</p>
+                <p><strong>Plekken beschikbaar:</strong> {{ $internship->spaces_left }}</a></p>
+                <p><strong>Stagevergoeding:</strong> {{ $internship->compensation }}</p>
+                <br>
+                <a class="btn btn-primary" href="{{ route('internships.show', $internship->id) }}"><button>Aanmelden</button></a>
+            </div>
         </div>
     </section>
 @endsection
